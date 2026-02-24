b7aa6ba3b4768e4663034944b8884bc082ca7270
2471949 <2471949@LTIN463203>
2026-02-24 17:25:35 +0530

    added action methods for enrollment table

---
+++ EduTrackAcademics/Exception/EnrollmentAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿namespace EduTrackAcademics.Exception
+{
+	public class EnrollmentAlreadyExistsException: ApplicationException
+	{
+		public int StatusCode { get; }
+
+		public EnrollmentAlreadyExistsException(string message, int statusCode = 500) : base(message)
+		{
+			StatusCode = statusCode;
+		}
+	}
+}
