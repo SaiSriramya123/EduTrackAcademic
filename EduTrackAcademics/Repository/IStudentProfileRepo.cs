2c02fae17208b1452faa35a06a7f4e3859b2d495
Lakshmi <2472021@cognizant.com>
2026-02-25 01:52:25 +0530

    Add changes

---
+++ EduTrackAcademics/Repository/IStudentProfileRepo.cs
@@ -0,0 +1,17 @@
+﻿using EduTrackAcademics.DTO;
+
+namespace EduTrackAcademics.Repository
+{
+	public interface IStudentProfileRepo
+	{
+		
+		public interface IStudentProfileRepo
+		{
+			Task<StudentDTO?> GetProgramDetailsAsync(string studentId);
+			Task<StudentDTO?> GetPersonalInfoAsync(string studentId);
+			Task<bool> StudentExistsAsync(string studentId);
+			Task UpdateAdditionalInfoAsync(string studentId, StudentAdditionalDetailsDTO dto);
+		}
+	}
+}
+
